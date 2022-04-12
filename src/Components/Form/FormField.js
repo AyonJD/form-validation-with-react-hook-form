04770d8099954a5fb032066f3135e5fc05383626
Ayon <ayonjodder177@gmail.com>
2022-04-12 16:29:05 +0600

    Name field error handle done

--- src/Components/Form/FormField.js
+++ src/Components/Form/FormField.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import './FormField.css'
 
 const FormField = () => {
+    // register => register function er fitore field er name r condition + message gula hobe
+    // handleSubmit => handleSubmit form submit er jonno use hobe
+    // reset => reset function input field clear er jonno use hobe
+    // formState: {errors} => formState ta error object ta rakhar jonno use hobe
+    // trigger => trigger function onKeyUp a use korle aita auto input field ta k trigger kore live error show korbe
+    const { register, handleSubmit, reset, formState: { errors }, trigger } = useForm();
+
+    //useForm hook er handleSubmit function er parameter----->
+    const onSubmitParam = data => {
+        console.log(data);
+        //Input field empty---->
+        reset()
+    }
     return (
         <div>
-            <Form className='w-50 mx-auto my-5 border px-4 pt-4 pb-5 rounded'>
+            {/* onSubmit er vitor hook er prebuild funtion pass korte hobe r tar parameter hobe 1 ta function, jeta input fueld er data gula k dhorbe*/}
+            <Form onSubmit={handleSubmit(onSubmitParam)} className='w-50 mx-auto my-5 border px-4 pt-4 pb-5 rounded'>
                 <h2 className='mb-4 text-center'>Please Register to Continue</h2>
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Full Name" />
+                    <Form.Control className={`rmv-shadow ${errors?.name && "invalid"}`} type="text" placeholder="Full Name" {...register("name", { required: 'Name is Required', minLength: { value: 4, message: 'Minimum 4 character required' } })}
+                        // register function er fitor condition r message gula object akare likhte hoy
+                        onKeyUp={() => {
+                            trigger('name')
+                        }}
+                    />
+                    <small className='text-danger'>{errors?.name?.message}</small>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
